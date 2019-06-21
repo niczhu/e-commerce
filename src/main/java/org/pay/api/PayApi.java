5e53954eafdb010aff5c1efc8f6758ea31d2f8ef
zhuhaipeng <dell@qq.com>
2019-06-21 16:42:49 +0800

    update

---
+++ src/main/java/org/pay/api/PayApi.java
@@ -0,0 +1,10 @@
+package org.pay.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PayApi {
+
+
+
+}
---
+++ src/main/java/org/pay/service/PayService.java
@@ -0,0 +1,4 @@
+package org.pay.service;
+
+public class PayService {
+}
